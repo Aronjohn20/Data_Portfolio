e1f40560d00c6686b7a7a30a1fa82d87c43ce3b4
Aronjohn20 <aronjohn12112004@gmail.com>
2026-03-26 01:25:37 +0530

    changed the pic

--- port.py
+++ port.py
@@ -17,7 +17,7 @@ EMAIL       = "aronjohn12112004@gmail.com"
 LINKEDIN    = "www.linkedin.com/in/aron-john27"
 GITHUB      = "https://github.com/Aronjohn20/Aronjohn20"
 RESUME_URL  = "https://drive.google.com/drive/folders/15_RRig9nWpqGERHO2WGEbcDbIGdb0tQZ?usp=drive_link"           # ← paste Google Drive / Dropbox PDF link
-PHOTO_URL   = "pic.jpeg"           # ← paste a direct image URL or leave empty
+PHOTO_URL   = "https://github.com/Aronjohn20/Data_Portfolio/blob/main/pic.jpeg"           # ← paste a direct image URL or leave empty
 
 ABOUT_TEXT = """
 Hi! I'm Aron — a aspiring data analyst obsessed with finding the story and solutions inside messy datasets.
@@ -655,6 +655,23 @@ if PAGE == "home":
 #  PAGE 2 — PROJECTS
 # ══════════════════════════════════════════════
 elif PAGE == "projects":
+     st.markdown("""
+    <style>
+    .proj-link {
+        position: relative;
+        z-index: 999;
+    }
+
+    .proj-link a {
+        pointer-events: auto !important;
+        position: relative;
+        z-index: 1000;
+        color: #00FFB2;
+        text-decoration: none;
+        font-weight: 600;
+    }
+    </style>
+    """, unsafe_allow_html=True)
     st.markdown('<div class="page">', unsafe_allow_html=True)
  
     st.markdown("""
